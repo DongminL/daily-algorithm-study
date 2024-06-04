f5c411a8ef43f4e4783b0a7d95355d3fb3cc94b7
Lee Dongmin <108854865+DongminL@users.noreply.github.com>
2024-06-04 23:42:29 +0900

    [Bronze III] Title: 팩토리얼, Time: 204 ms, Memory: 17704 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.java
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -3,11 +3,16 @@ class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int n;
+        
         n = s.nextInt();
+        
         System.out.print(factorial(n));
     }
+    
     static int factorial(int n){
-    if (n == 0 || n == 1) return 1;
-    return n * factorial(n-1);
-}
+        if (n == 0 || n == 1) 
+            return 1;
+        
+        return n * factorial(n-1);
+    }
 }
