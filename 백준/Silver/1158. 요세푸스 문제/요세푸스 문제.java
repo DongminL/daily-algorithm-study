bd04a794540d4e52c7653d9d8bf0cc69e3e05d64
Lee Dongmin <108854865+DongminL@users.noreply.github.com>
2024-06-07 16:48:23 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 164 ms, Memory: 14580 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        LinkedList<Integer> circle = new LinkedList<>();
+
+        for(int i = 0; i < n; i++) {
+            circle.add(i+1);
+        }
+
+        sb.append("<");
+        int cnt = 0;
+        int current;
+        while (n > 1) {
+            cnt = (cnt + (k - 1)) % n;
+            current = circle.remove(cnt);
+
+            sb.append(current).append(", ");
+
+            n--;
+        }
+        sb.append(circle.remove(0));
+        sb.append(">");
+
+        System.out.println(sb);
+    }
+}
