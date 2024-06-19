588253bd68e1b02af08c50fb5248cbbfb012deec
Lee Dongmin <milkskfk12@gmail.com>
2024-06-19 22:37:27 +0900

    [Silver III] Title: 두 수의 합, Time: 424 ms, Memory: 28156 KB -BaekjoonHub

---
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        int n = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        int x = Integer.parseInt(br.readLine());
+        
+        int[] values = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            values[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        Arrays.sort(values);
+        
+        int start = 0;
+        int end = n-1;
+        int cnt = 0;
+        int sum = 0;
+        
+        while (start < end && (values[end] > x/2.0)) {
+            sum = values[start] + values[end];
+            
+            if (sum == x) {
+                cnt++;
+                start++;
+                end--;
+            } else if (sum < x) {
+                start++;
+            } else {
+                end--;
+            }
+        }
+        
+        System.out.println(cnt);
+    }
+}
