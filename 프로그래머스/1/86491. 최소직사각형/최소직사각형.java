cca86296e86faa4b92b645e6fa768d5dceb19767
Lee Dongmin <milkskfk12@gmail.com>
2024-08-29 18:28:55 +0900

    [level 1] Title: 최소직사각형, Time: 1.67 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -4,21 +4,17 @@ import java.util.stream.IntStream;
 class Solution {
     public int solution(int[][] sizes) {
         int answer = 0;
-        List<Integer> biggerList = new ArrayList<>();   // 가로와 높이 중 더 긴 쪽의 길이들
-        List<Integer> shorterList = new ArrayList<>();   // 가로와 높이 중 더 짧은 쪽의 길이들
+        int longer = 0; // 가로와 세로 중 긴 부분
+        int shorter = 0;    // 가로와 세로 중 짧은 부분
         
+        // 긴쪽 중에서 최대 값과 짧은 쪽 중에서 최대값을 구함
         for (int i = 0; i < sizes.length; i++) {
-            int max = IntStream.of(sizes[i]).max().orElse(0); // 둘 중 큰 길이
-            int min = IntStream.of(sizes[i]).min().orElse(0); // 둘 중 짧은 길이
-            
-            biggerList.add(max);
-            shorterList.add(min);
+            longer = Math.max(longer, Math.max(sizes[i][0], sizes[i][1]));
+            shorter = Math.max(shorter, Math.min(sizes[i][0], sizes[i][1]));
         }
         
         // 긴쪽 중에서 최대 값과 짧은 쪽 중에서 최대값을 곱함
-        int biggerMax = biggerList.stream().max(Comparator.naturalOrder()).orElse(0);
-        int shorterMax = shorterList.stream().max(Comparator.naturalOrder()).orElse(0);
-        answer = biggerMax * shorterMax ;
+        answer = longer * shorter;
         
         return answer;
     }
