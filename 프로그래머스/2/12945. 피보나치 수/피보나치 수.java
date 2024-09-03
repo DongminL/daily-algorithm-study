d8024794ae883cde35e7a9bc866ce77694ca75e8
Lee Dongmin <milkskfk12@gmail.com>
2024-09-03 20:14:25 +0900

    [level 2] Title: 피보나치 수, Time: 4.47 ms, Memory: 71.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int n) {
+        long answer = 1; // F(n)
+        long n1 = 1; // F(n-1)
+        long n2 = 0; // F(n-2)
+        final int DIV = 1234567;
+        
+        // n일 때 피보나치 수 구하기
+        for (int i = 3; i <= n; i++) {
+            n2 = n1;
+            n1 = answer;
+            
+            answer = ((n1 % DIV) + (n2 % DIV)) % DIV;
+        }
+        
+        return (int) answer;
+    }
+}
