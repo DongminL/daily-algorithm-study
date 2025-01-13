8789040af75af124027d4f6a673b6a3e6f06ceca
Lee Dongmin <milkskfk12@gmail.com>
2025-01-13 21:21:32 +0900

    [level 3] Title: 합승 택시 요금, Time: 187.68 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -0,0 +1,81 @@
+import java.util.*;
+
+class Solution {
+    
+    private List<Taxi>[] routes; // 택시 경로에 대한 인접 리스트
+    
+    public int solution(int n, int s, int a, int b, int[][] fares) {
+        int answer = Integer.MAX_VALUE; // 최적 예상 택시요금
+        
+        // 인접 리스트 초기화
+        routes = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) {
+            routes[i] = new ArrayList<>();
+        }
+        
+        // 양방향 엣지로 추가
+        for (int[] fare : fares) {
+            routes[fare[0]].add(new Taxi(fare[1], fare[2]));
+            routes[fare[1]].add(new Taxi(fare[0], fare[2]));
+        }
+        
+        // 합승 택시 요금의 최소 요금 구하기
+        int[] commonRoute = dijkstra(n, s);
+        for (int i = 1; i <= n; i++) {
+            int[] leaveLocationFare = dijkstra(n, i);   // i 지점에서 서로 떠났을 때
+            
+            // 출발지부터 i 지역까지 같이 타고, i 지역부터 따로 갈 때의 비용
+            int fare = commonRoute[i] + leaveLocationFare[a] + leaveLocationFare[b];
+            answer = Math.min(answer, fare);
+        }
+        
+        return answer;
+    }
+    
+    // 최소 요금 구하기
+    private int[] dijkstra(int n, int start) {
+        // 각 지점까지의 최소 비용
+        int[] minimumFares = new int[n + 1];
+        Arrays.fill(minimumFares, 20_000_001);
+        
+        Queue<Taxi> pq = new PriorityQueue<>();
+        
+        // 시작 지점 설정
+        pq.offer(new Taxi(start, 0));
+        minimumFares[start] = 0;
+        
+        while (!pq.isEmpty()) {
+            Taxi current = pq.poll();   // 현재 위치
+            
+            // 더 적은 비용으로 올 수 있는 경우는 패스
+            if (minimumFares[current.location] < current.fare) {
+                continue;
+            }
+            
+            for (Taxi next : routes[current.location]) {
+                // 최소 요금이 갱신되는 경우에만 탐색
+                if (minimumFares[current.location] + next.fare < minimumFares[next.location]) {
+                    minimumFares[next.location] = minimumFares[current.location] + next.fare;
+                    pq.offer(new Taxi(next.location, minimumFares[next.location]));
+                }
+            }
+        }
+        
+        return minimumFares;
+    }
+    
+    static class Taxi implements Comparable<Taxi> {
+        
+        int location;    // 위치
+        int fare;   // 요금
+        
+        public Taxi(int location, int fare) {
+            this.location = location;
+            this.fare = fare;
+        }
+        
+        public int compareTo(Taxi o) {
+            return Integer.compare(this.fare, o.fare);
+        }
+    }
+}
