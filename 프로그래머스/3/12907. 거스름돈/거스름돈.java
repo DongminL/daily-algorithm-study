33bbb0a6974d2f9f995dfa833801bfce39124f99
Lee Dongmin <milkskfk12@gmail.com>
2025-01-30 19:27:57 +0900

    [level 3] Title: 거스름돈, Time: 19.44 ms, Memory: 69.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int[] money) {
+        int answer = 0; // n원을 거슬러줄 방법의 수
+        int DIVIDING_VALUE = 1_000_000_007; // 나누는 값
+        
+        // i원을 만드는 경우의 수
+        int[] dp = new int[Arrays.stream(money).sum() + 1];
+        dp[0] = 1;  // 0원을 만드는 경우의 수는 무조건 하나
+        
+        // 누적하여 경우의 수 구하기
+        for (int won : money) {
+            for (int current = won; current <= n; current++) {
+                // i원을 만드는 경우의 수 : 
+                // (현재 만들려는 돈의 경우의 수 - 해당 동전을 만드는 경우의 수)
+                dp[current] += dp[current - won];   
+            }
+        }
+        
+        answer = dp[n] % DIVIDING_VALUE;
+        return answer;
+    }
+}
