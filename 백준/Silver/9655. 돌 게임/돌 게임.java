f264f5515ef1d3e85802eb50c38320800b221ee7
Lee Dongmin <milkskfk12@gmail.com>
2025-06-06 22:57:05 +0900

    [Silver V] Title: 돌 게임, Time: 100 ms, Memory: 14392 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.java
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -3,11 +3,25 @@ import java.io.*;
 class Solution {
 
     String solution(int n) {
-        if (n % 2 == 1) {
-            return "SK";
-        } else {
-            return "CY";
+        // index : 돌의 개수
+        // true : 상근이 승리, false : 창영이 승리 (상근이 패배)
+        boolean[] winners = new boolean[n+1];
+        winners[1] = true;
+
+        if (n >= 2) {
+            winners[2] = false;
         }
+        if (n >= 3) {
+            winners[3] = true;
+        }
+
+        for (int i = 4; i <= n; i++) {
+            // 상근이가 먼저 가져간 후, 남은 개수에서 창영이가 먼저 가져가기 때문에
+            // 해당 개수의 기록에서 반대 결과로 결정됨
+            winners[i] = !winners[i - 1] || !winners[i - 3];
+        }
+
+        return winners[n] ? "SK" : "CY";
     }
 }
 
