c00506adeeacf72a20ab5827f9687fe6924fd8d7
Lee Dongmin <milkskfk12@gmail.com>
2025-07-07 18:36:44 +0900

    [level 1] Title: 체육복, Time: 1.46 ms, Memory: 93.4 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,20 +1,24 @@
 import java.util.*;
-import java.util.stream.Collectors;
+import java.util.stream.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int answer = n - lost.length;   // 수업을 들을 수 있는 최대 학생의 수
-        boolean[] isBorrow = new boolean[n+2];     // 각 학생이 빌려줄 수 있는 체육복의 여부 (양 끝 학생이 좌우를 탐색하기 위함)
+        // 수업을 들을 수 있는 최대 학생의 수
+        int answer = n - lost.length;
+        // 각 학생이 빌려줄 수 있는 체육복의 여부 (양 끝 학생이 좌우를 탐색하기 위함)
+        boolean[] isBorrow = new boolean[n+2];
         
         // 체육복 여부 갱신
-        Arrays.stream(reserve).forEach(student -> isBorrow[student] = true);
+        Arrays.stream(reserve)
+            .forEach(student -> isBorrow[student] = true);
         
         // 여벌이 있는 사람이 도둑 맞았을 때, 자신의 여벌 입기
-         List<Integer> lostList = new ArrayList<>();    // 여벌 있는 사람이 도둑 맞은 경우 제외한 리스트
+         List<Integer> lostList = new ArrayList<>();    // 여벌 있는 사람이 도둑 맞은 경우를 제외한 리스트
         for (int s : lost) {
             if (isBorrow[s]) {
                 isBorrow[s] = false;
                 answer++;
+                
             } else {
                 lostList.add(s);
             }
