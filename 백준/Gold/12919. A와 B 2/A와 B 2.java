a6a5788ff4777afc833c3519c1ba2550c0affaae
Lee Dongmin <milkskfk12@gmail.com>
2025-08-06 21:01:21 +0900

    [Gold V] Title: A와 B 2, Time: 104 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Gold/12919. A와 B 2/A와 B 2.java
+++ 백준/Gold/12919. A와 B 2/A와 B 2.java
@@ -1,36 +1,34 @@
 import java.io.*;
-import java.util.*;
 
 class Solution {
 
-    int answer; // S를 T로 바꿀 수 있으면 1을 없으면 0
+    // s를 t로 바꿀 수 있는 여부
+    private boolean answer = false;
 
-    public int solution(String S, String T) {
-        answer = 0;
-        dfs(S, S.length(), T, T.length());
+    int solution(String s, String t) {
+        makeTToS(s, t);
 
-        return answer;
+        return answer ? 1 : 0;
     }
 
-    /* 두 가지의 연산을 통해 T를 S로 만들기 */
-    private void dfs(String S, int length, String cur, int curLength) {
-        // 문자열의 길이가 같아질 때까지만 만들기
-        if (curLength == length) {
-            if (S.equals(cur)) {
-                answer = 1;
+    // t 문자열에서 s로 만들기
+    private void makeTToS(String s, String t) {
+        if (s.length() == t.length()) {
+            if (s.equals(t)) {
+                answer = true;
             }
             return;
         }
 
-        // 순서를 알지 못함으로 각 경우를 따로 진행함으로써 확인
-        // A를 추가하는 연산을 거꾸로
-        if (cur.charAt(curLength - 1) == 'A') {
-            dfs(S, length, cur.substring(0, curLength-1), curLength - 1);
+        // A가 추가되었을 때 되돌리기
+        if (t.charAt(t.length() - 1) == 'A') {
+            makeTToS(s, t.substring(0, t.length() - 1));
         }
-        // B를 추가하는 연산을 거꾸로
-        if (cur.charAt(0) == 'B') {
-            cur = new StringBuilder(cur.substring(1)).reverse().toString();
-            dfs(S, length, cur, curLength - 1);
+
+        // B가 추가되었을 때 되돌리기
+        if (t.charAt(0) == 'B') {
+            StringBuilder next = new StringBuilder(t);
+            makeTToS(s, next.delete(0, 1).reverse().toString());
         }
     }
 }
@@ -40,9 +38,9 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String S = br.readLine();    // 문자열 S
-        String T = br.readLine();    // 문자열 T
+        String s = br.readLine();
+        String t = br.readLine();
 
-        System.out.println(new Solution().solution(S, T));;
+        System.out.println(new Solution().solution(s, t));
     }
 }
