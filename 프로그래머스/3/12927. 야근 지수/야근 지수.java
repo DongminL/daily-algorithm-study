eef65731bce287df970d73a323ff42565497bce8
Lee Dongmin <milkskfk12@gmail.com>
2025-08-20 23:43:23 +0900

    [level 3] Title: 야근 지수, Time: 133.69 ms, Memory: 70.8 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.java
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -2,25 +2,25 @@ import java.util.*;
 
 class Solution {
     public long solution(int n, int[] works) {
-        long answer = 0;    // 야근 피로도의 최소값
+        // 야근 피로도의 최소값
+        long answer = 0;    
         
         // 내림차순으로 정렬
         PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
-        Arrays.stream(works).forEach(maxHeap::offer);
+        for (int work : works) {
+            maxHeap.offer(work);
+        }
         
-        // 남은 작업들의 평균을 작게 만들기
-        while (n > 0) {
-            maxHeap.offer(maxHeap.poll() - 1);  // 1시간 작업하고 다시 추가
-            
-            n--;
+        // 많이 남은 작업을 우선으로 처리하기 (평균 줄이는 목적)
+        for (int i = 0; i < n; i++) {
+            maxHeap.offer(maxHeap.poll() - 1);
         }
         
         // 야근 피로도 구하기
-        for (int work : maxHeap) {
-            if (work > 0) {
-                answer += Math.pow(work, 2);
-            }
-        }
+        answer = maxHeap.stream()
+            .filter(remain -> remain > 0)
+            .mapToLong(remain -> remain * remain)
+            .sum();
         
         return answer;
     }
