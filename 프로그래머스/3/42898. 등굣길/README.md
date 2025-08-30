a314eb51608cae00501b1030a233f896ae0514ef
Lee Dongmin <milkskfk12@gmail.com>
2025-08-30 19:22:38 +0900

    [level 3] Title: 등굣길, Time: 1.24 ms, Memory: 54.7 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/README.md
+++ 프로그래머스/3/42898. 등굣길/README.md
@@ -1,10 +1,10 @@
 # [level 3] 등굣길 - 42898 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42898?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42898) 
 
 ### 성능 요약
 
-메모리: 69.1 MB, 시간: 3.72 ms
+메모리: 54.7 MB, 시간: 1.24 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 20일 21:23:21
+2025년 08월 30일 19:22:35
 
 ### 문제 설명
 
--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -3,25 +3,27 @@ import java.util.*;
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
         // 물에 잠긴 지역이 포함된 지도 (물에 잠긴 곳: -1)
-        int[][] map = new int[n+1][m+1];
-        Arrays.stream(puddles)
-            .forEach(waterCoor -> map[waterCoor[1]][waterCoor[0]] = -1);
+        int[][] map = new int[n + 1][m + 1];
+        for (int[] puddle : puddles) {
+            map[puddle[1]][puddle[0]] = -1;
+        }
+        
+        // 집의 위치 설정
+        map[1][1] = 1;
         
         // 각 좌표까지 갈 수 있는 경우의 수를 계산
         for (int y = 1; y <= n; y++) {
             for (int x = 1; x <= m; x++) {
-                // 초기값 지정 (집의 위치)
-                if (x == 1 && y == 1) {
-                    map[y][x] = 1;
+                // 집과 물에 잠긴 곳은 패스
+                if ((y == 1 && x == 1) || map[y][x] == -1) {
                     continue;
                 }
                 
-                if (map[y][x] != -1) {
-                    // 현재 위치에서 왼쪽과 위쪽에 물에 잠긴 지역이 있을 수 있으므로
-                    // -1(물에 잠긴 지역)을 0으로 변환하여 경우의 수 계산에 지장이 없도록 함
-                    map[y][x] = Math.max(map[y][x-1], map[y][x]) + Math.max(map[y-1][x], map[y][x]);
-                    map[y][x] %= 1000000007;
-                }
+                // 현재 위치에서 왼쪽과 위쪽에 물에 잠긴 지역이 있을 수 있으므로
+                // -1(물에 잠긴 지역)을 0으로 변환하여 경우의 수 계산에 지장이 없도록 함
+                int west = Math.max(map[y][x - 1], map[y][x]);
+                int north = Math.max(map[y - 1][x], map[y][x]);
+                map[y][x] = (west + north) % 1_000_000_007;
             }
         }
         
