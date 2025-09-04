5252ccc380ee661ddbc8124dd2504d1ada195924
Lee Dongmin <milkskfk12@gmail.com>
2025-09-04 22:02:39 +0900

    [Gold V] Title: 용액, Time: 328 ms, Memory: 30380 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.java
+++ 백준/Gold/2467. 용액/용액.java
@@ -3,39 +3,36 @@ import java.util.*;
 
 class Solution {
 
-    int[] solution(int n, int[] concentration) {
+    int[] solution(int n, int[] values) {
         // 특성값이 0에 가장 가까운 용액을 만들어내는 두 용액의 특성값
         int[] answer = new int[2];
 
-        // 가장 0에 가까운 농도
-        int min = Integer.MAX_VALUE;
+        int minValue = Integer.MAX_VALUE;   // 혼합한 용액의 최소 특성값 (절댓값)
 
-        // Two Pointer
         int left = 0;
         int right = n - 1;
 
-        // 0에 가장 가깝게 만드는 두 용액 찾기
+        // 0에 가깝게 되는 두 용액 찾기
         while (left < right) {
-            int mixed = concentration[left] + concentration[right];
+            int mixedValue = values[left] + values[right];  // 혼합한 용액의 특성값
 
-            // 섞었을 때 0에 가까운 두 용액 갱신
-            if (Math.abs(mixed) < min) {
-                min = Math.abs(mixed);
+            // 최소값 갱신
+            if (Math.abs(mixedValue) < minValue) {
+                minValue = Math.abs(mixedValue);
 
-                answer[0] = concentration[left];
-                answer[1] = concentration[right];
+                answer[0] = values[left];
+                answer[1] = values[right];
 
-                // 0인 용액을 만들었다면 탐색 종료
-                if (min == 0) {
+                if (minValue == 0) {
                     break;
                 }
             }
 
-            // 0에 가까워지게 용액 교체
-            if (mixed < 0) {
-                left++;
-            } else {
+            // 농도에 따라 용액 변경
+            if (mixedValue > 0) {
                 right--;
+            } else {
+                left++;
             }
         }
 
@@ -49,11 +46,11 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        int[] concentration = Arrays.stream(br.readLine().split(" "))
+        int[] values = Arrays.stream(br.readLine().split(" "))
             .mapToInt(Integer::parseInt)
             .toArray();
 
-        int[] answer = new Solution().solution(n, concentration);
+        int[] answer = new Solution().solution(n, values);
         System.out.println(answer[0] + " " + answer[1]);
     }
 }
