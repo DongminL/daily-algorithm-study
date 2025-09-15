b72b4a0480523021aabc3c67fa8f257a4e7a723e
Lee Dongmin <milkskfk12@gmail.com>
2025-09-15 19:38:54 +0900

    [Gold IV] Title: 좋다, Time: 160 ms, Memory: 15016 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -3,43 +3,63 @@ import java.util.*;
 
 class Solution {
 
-    public int solution(int n, int[] nums) {
-        int answer = 0; // 좋은 수의 개수
+    private final int n;
+    private final int[] nums;
 
-        // 주어진 수열을 오름차순 정렬
+    public Solution(int n, int[] nums) {
+        this.n = n;
+        this.nums = nums;
+    }
+
+    int solution() {
+        // 좋은 수의 개수
+        int answer = 0;
+        
+        // 수열을 오름차순으로 정렬
         Arrays.sort(nums);
 
+        // i번째 수가 좋은 수인지 탐색
         for (int i = 0; i < n; i++) {
-            int left = 0;   // 가장 작은 값의 인덱스
-            int right = n - 1;  // 가장 큰 값의 인덱스
-
-            // Binary Search
-            while (left < right) {
-                // 다른 수로 i 번째 값을 만들어야 하기 때문에 둘 중 같은 수면 패스
-                if (i == left) {
-                    left++;
-                    continue;
-                } else if (i == right) {
-                    right--;
-                    continue;
-                }
-
-                // 좋다 수일 때
-                if (nums[left] + nums[right] == nums[i]) {
-                    answer++;
-                    break;
-                }
-
-                // 좋다 수가 아닐 때 인덱스 이동
-                if (nums[left] + nums[right] < nums[i]) {
-                    left++;
-                } else {
-                    right--;
-                }
+            if (isGoodNumber(i)) {
+                answer++;
             }
         }
 
         return answer;
+    }
+
+    // 해당 위치의 숫자가 좋은 수인지 확인
+    private boolean isGoodNumber(int index) {
+        int left = 0;
+        int right = n - 1;
+        
+        while (left < right) {
+            // 더하는 수가 만드려는 수가 같은 경우, 다른 위치의 수로 변경
+            if (index == left) {
+                left++;
+                continue;
+            }
+            if (index == right) {
+                right--;
+                continue;
+            }
+
+            int sum = nums[left] + nums[right];
+
+            // 좋은 수 일 때
+            if (sum == nums[index]) {
+                return true;
+            }
+
+            // 좋은 수가 아닐 때
+            if (sum < nums[index]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return false;
     }
 }
 
@@ -48,10 +68,11 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());    // 수의 개수
-        int[] A = Arrays.stream(br.readLine().split(" "))   //  수열 A
-            .mapToInt(Integer::parseInt).toArray();
+        int n = Integer.parseInt(br.readLine());
+        int[] nums = Arrays.stream(br.readLine().split(" "))
+            .mapToInt(Integer::parseInt)
+            .toArray();
 
-        System.out.println(new Solution().solution(n, A));
+        System.out.println(new Solution(n, nums).solution());
     }
 }
