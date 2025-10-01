af05feb6536480dd5a486591abb88b71eacca7a9
Lee Dongmin <milkskfk12@gmail.com>
2025-10-01 20:32:55 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 56.93 ms, Memory: 197 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
@@ -2,18 +2,23 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] numbers) {
+        // 모든 원소에 대한 뒷 큰수들
         int[] answer = new int[numbers.length];
-        Deque<Integer> stack = new ArrayDeque<>();  // 뒷 큰 수 후보들을 담는 스택
         
-        // 뒷 큰 수 찾기
+        // 뒤에 있는 큰수 후보들을 담는 스택
+        Deque<Integer> stack = new ArrayDeque<>();
+        
+        // 뒷 큰수 찾기
         for (int i = numbers.length - 1; i > -1; i--) {
             while (!stack.isEmpty()) {
-                if (stack.peek() > numbers[i]) {    // 뒷 큰 수 존재할 때
+                // 뒷 큰 수 존재할 때
+                if (stack.peek() > numbers[i]) {
                     answer[i] = stack.peek();
                     break;
-                } else {    // 뒷 큰 수가 없을 때, 스택에서 작은 수 빼기
-                    stack.pollFirst();
                 }
+                
+                // 뒷 큰 수가 없을 때, 스택에서 작은 수 빼기
+                stack.pop();
             }
             
             // 뒷 큰 수가 없을 때
@@ -21,7 +26,7 @@ class Solution {
                 answer[i] = -1;   
             }
             
-            stack.offerFirst(numbers[i]);
+            stack.push(numbers[i]);
         }
         
         return answer;
