6d907faae7614110edd574e766528110b897bbb9
Lee Dongmin <milkskfk12@gmail.com>
2025-10-10 21:18:43 +0900

    [level 2] Title: 하노이의 탑, Time: 7.18 ms, Memory: 99.3 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/README.md
+++ 프로그래머스/2/12946. 하노이의 탑/README.md
@@ -1,10 +1,10 @@
 # [level 2] 하노이의 탑 - 12946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12946#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12946) 
 
 ### 성능 요약
 
-메모리: 92.6 MB, 시간: 17.31 ms
+메모리: 99.3 MB, 시간: 7.18 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 23일 18:44:03
+2025년 10월 10일 21:18:40
 
 ### 문제 설명
 
--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
@@ -2,11 +2,10 @@ import java.util.*;
 
 class Solution {
     
-    private List<int[]> answer; // n개의 원판을 3번 기둥으로 최소로 옮기는 방법
+    // n개의 원판을 3번 기둥으로 최소로 옮기는 방법
+    private List<int[]> answer = new ArrayList<>(); 
     
     public int[][] solution(int n) {
-        answer = new ArrayList<>();
-        
         // 원판의 개수와 각 기둥의 번호 지정
         hanoi(n, 1, 2, 3);
         
@@ -14,7 +13,7 @@ class Solution {
     }
     
     private void hanoi(int n, int start, int mid, int end) {
-        // 원판을 옮길 때 마지막 번째 때 기록
+        // 마지막 원판을 옮길 때, 기록
         if (n == 1) {
             answer.add(new int[] {start, end});
             return;
@@ -23,10 +22,10 @@ class Solution {
         // (전체 원판의 개수 - 1)개를 start -> mid 기둥으로 옮김
         hanoi(n-1, start, end, mid);
         
-        // 기둥에 하나의 원판만 남았을 경우
+        // 기둥에 남은 1개의 원판 기록
         answer.add(new int[] {start, end});
         
-        // 남아있는 원판(n-1개)을 end 기둥으로 옮김
+        // mid에 남아있는 원판(n-1개)을 end 기둥으로 옮김
         hanoi(n-1, mid, start, end);
     }
 }
