c754e48a997bbecb4805909eae4b8e6e372b84c2
Lee Dongmin <milkskfk12@gmail.com>
2025-10-20 17:57:37 +0900

    [level 2] Title: 프로세스, Time: 2.52 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -2,12 +2,14 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] priorities, int location) {
-        // 우선순위 큐에 삽입
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());  // 내림차순
+        // location에 위치한 프로세스의 실행 순서
+        int answer = 0; 
+        
+        // 내림차순 정렬
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());  
         Arrays.stream(priorities).forEach(pq::offer);
         
         // location에 위치한 프로세스의 실행 알아내기
-        int answer = 0; // location에 위치한 프로세스의 실행 순서
         while (!pq.isEmpty()) {
             for (int i = 0; i < priorities.length; i++) {
                 if (priorities[i] == pq.peek()) {
