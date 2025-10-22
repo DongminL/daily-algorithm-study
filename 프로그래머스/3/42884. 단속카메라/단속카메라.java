4bec4520867dad2ca138107923f01a5c649fcb76
Lee Dongmin <milkskfk12@gmail.com>
2025-10-22 18:58:13 +0900

    [level 3] Title: 단속카메라, Time: 12.58 ms, Memory: 58.7 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -2,20 +2,21 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] routes) {
-        int answer = 0; // 최소 단속 카메라 수
+        // 최소 단속 카메라 수
+        int answer = 0; 
         
         // 나간 지점을 기준으로 오름차순 정렬
-        Arrays.sort(routes, (r1, r2) -> r1[1] - r2[1]);
+        Arrays.sort(routes, (r1, r2) -> Integer.compare(r1[1], r2[1]));
         
         // 첫번째 단속 카메라 설치
         int camera = routes[0][1];    // 카메라를 설치한 지점
         answer++;
         
-        // 단속 카메라 추가
+        // 단속 카메라 추가하기
         for (int[] route : routes) {
-            // 카메라를 못 만날 경우
+            // 카메라를 못 만날 경우, 카메라 위치 갱신
             if (camera < route[0]) {
-                camera = route[1];  // 추가한 카메라 위치 갱신
+                camera = route[1];
                 answer++;
             }
         }
