f6abb32920c2e8d68b2e8f3060cbac9f9dbf72f7
Lee Dongmin <milkskfk12@gmail.com>
2025-10-23 20:57:39 +0900

    [level 3] Title: 가장 먼 노드, Time: 58.75 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -2,12 +2,16 @@ import java.util.*;
 
 class Solution {
     
-    ArrayList<Integer>[] list;  // 인접 리스트
-    boolean[] visited;  // 각 노드에 방문한 여부
+    // 인접 리스트
+    ArrayList<Integer>[] list;
+    // 각 노드에 방문한 여부
+    boolean[] visited;
     
     public int solution(int n, int[][] edge) {
-        // 초기화
-        list = new ArrayList[n+1];
+        // 1번 노드에서 가장 멀리 떨어진 노드의 개수
+        int answer = 0; 
+        
+        list = new ArrayList[n + 1];
         for (int i = 1; i <= n; i++) {
             list[i] = new ArrayList<>();
         }
@@ -18,15 +22,16 @@ class Solution {
             list[e[0]].add(e[1]);
             list[e[1]].add(e[0]);
         }
+    
+        int[] moveCount = countMoves(1, n);
+        int max = 0;    // 1번 노드에서 최대로 이동한 횟수
         
         // 가장 멀리 떨어진 노드의 개수 찾기
-        int answer = 0; // 1번 노드에서 가장 멀리 떨어진 노드의 개수
-        int max = 0;    // 1번 노드에서 최대로 이동한 횟수
-        int[] moveCount = bfs(1, n);
         for (int i = 2; i <= n; i++) {
             if (max < moveCount[i]) {
                 answer = 1;
                 max = moveCount[i];
+                
             } else if (max == moveCount[i]) {
                 answer++;
             }
@@ -35,23 +40,26 @@ class Solution {
         return answer;
     }
     
-    /* start -> end까지의 이동한 수 세기 (BFS) */
-    private int[] bfs(int start, int n) {
-        int[] moveCount = new int[n+1];    // 1번에서 출발하여 각 노드까지 움직인 횟수
-        Deque<Integer> queue = new ArrayDeque<>();
-        visited = new boolean[n+1];
+    // start -> end까지의 이동한 수 세기 (BFS)
+    private int[] countMoves(int start, int n) {
+        int[] moveCount = new int[n + 1];    // 1번에서 출발하여 각 노드까지 움직인 횟수
         
-        queue.offerLast(1);
+        Queue<Integer> queue = new ArrayDeque<>();
+        visited = new boolean[n + 1];
+        
+        // 출발 지점을 1번으로 설정
+        queue.offer(1);
         visited[start] = true;
         
         while (!queue.isEmpty()) {
-            int cur = queue.pollFirst();    // 현재 노드
+            int current = queue.poll();
             
-            // 다음 노드 방문
-            for (int next : list[cur]) {
-                if (!visited[next]) {   // 방문하지 않은 노드만
-                    queue.offerLast(next);
-                    moveCount[next] = moveCount[cur] + 1;   // 움직인 횟수 갱신
+            // 다음 노드 방문 
+            for (int next : list[current]) {
+                // 중복 방문은 제외
+                if (!visited[next]) {
+                    queue.offer(next);
+                    moveCount[next] = moveCount[current] + 1;   // 움직인 횟수 갱신
                     visited[next] = true;
                 }
             }
