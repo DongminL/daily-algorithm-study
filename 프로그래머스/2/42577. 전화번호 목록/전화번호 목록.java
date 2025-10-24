41239c8ae46efccbc421209eedb2c384a35802e8
Lee Dongmin <milkskfk12@gmail.com>
2025-10-24 21:01:33 +0900

    [level 2] Title: 전화번호 목록, Time: 287.31 ms, Memory: 99.5 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,13 +2,15 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
+        // 어떤 번호가 다른 번호의 접두어인 여부
         boolean answer = true;
         
-        Arrays.sort(phone_book);    // 오름차순 정렬
+        // 오름차순 정렬
+        Arrays.sort(phone_book);
         
         // 다음 문자열의 접두어가 현재 문자열로 시작하는지 검사
         for (int i = 0; i < phone_book.length - 1; i++) {
-            if (phone_book[i+1].startsWith(phone_book[i])) {
+            if (phone_book[i + 1].startsWith(phone_book[i])) {
                 answer = false;
                 break;
             }
