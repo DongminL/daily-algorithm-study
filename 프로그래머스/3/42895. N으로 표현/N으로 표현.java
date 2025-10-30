dc0d8cf8b7aaa19c326dd7a73f59559dd8ae2fe7
Lee Dongmin <milkskfk12@gmail.com>
2025-10-30 20:43:11 +0900

    [level 3] Title: N으로 표현, Time: 57.49 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -1,16 +1,15 @@
 class Solution {
     
-    int answer; // N 사용횟수의 최솟값
+    // N 사용 횟수의 최솟값
+    int answer = 9; 
     
     public int solution(int N, int number) {
-        answer = 9; 
-        
         express(N, 0, number, 0);
         
         return answer > 8 ? -1 : answer;
     }
     
-    /* N과 사칙연산으로 target을 표현 (DFS) */
+    // N과 사칙연산으로 target을 표현 (DFS)
     private void express(int N, int cur, int target, int count) {
         // 최솟값 갱신이 안될 때
         if (count > 8) {
@@ -26,7 +25,7 @@ class Solution {
         int nNum = 0;    // N으로만 구성된 n자리수
         for (int i = 1; i < 6; i++) {
             // 일의 자리에 N을 추가
-            nNum = nNum*10 + N;  
+            nNum = nNum * 10 + N;  
             
             // 덧셈
             express(N, cur + nNum, target, count + i);
@@ -37,6 +36,7 @@ class Solution {
             if (cur != 0) {
                 // 곱셈
                 express(N, cur * nNum, target, count + i);
+                
                 // 나눗셈
                 express(N, cur / nNum, target, count + i);
                 express(N, nNum / cur, target, count + i);
