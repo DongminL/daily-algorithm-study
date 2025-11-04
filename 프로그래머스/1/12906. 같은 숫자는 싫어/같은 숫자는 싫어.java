e4ef670000cbb74814799bbe680958ec35c7412a
Lee Dongmin <milkskfk12@gmail.com>
2025-11-04 14:10:44 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 56.31 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -2,19 +2,20 @@ import java.util.*;
 
 public class Solution {
     public int[] solution(int[] arr) {
+        // 배열 arr에서 연속적으로 나타나는 숫자는 제거하고 남은 수들
         List<Integer> answer = new ArrayList<>();
-        Deque<Integer> stack = new ArrayDeque<>();
         
-        // 연속적으로 나타나는 숫자만 answer에 추가
-        stack.offerFirst(arr[0]);
-        Arrays.stream(arr)
-            .skip(1)
-            .filter(num -> stack.peek() != num)
-            .forEach(num -> {
-                answer.add(stack.pollFirst());
-                stack.offerFirst(num);
-            });
-        answer.add(stack.pollFirst());  // 마지막 연속값 추가
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.push(arr[0]);
+        
+        // 연속적으로 나타나는 숫자들의 중복 제거
+        for (int i = 1; i < arr.length; i++) {
+            if (!stack.isEmpty() && stack.peek() != arr[i]) {
+                answer.add(stack.pop());
+                stack.push(arr[i]);
+            }
+        }
+        answer.add(stack.pop());  // 마지막 숫자 추가
 
         return answer.stream().mapToInt(i -> i).toArray();
     }
