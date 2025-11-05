18e8d59d2bb1332413a49398b4819297827fe5fd
Lee Dongmin <milkskfk12@gmail.com>
2025-11-05 20:03:19 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 97.40 ms, Memory: 96.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java) 
 
 ### 성능 요약
 
-메모리: 95.4 MB, 시간: 86.94 ms
+메모리: 96.8 MB, 시간: 97.40 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 27일 21:26:46
+2025년 11월 05일 20:03:15
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,15 +2,22 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        // 마라톤 선수가 완주한 여부를 체크하는 목록 생성 (기본값 0, 동명이인은 +1)
-        Map<String, Integer> checking = new HashMap<>();
-        Arrays.stream(participant).forEach(player -> checking.put(player, checking.getOrDefault(player, 0) + 1));
+        // 완주하지 못한 사람
+        String answer = "";
         
-        // 완주자 체킹 (완주자의 값: -1)
-        Arrays.stream(completion).forEach(completed -> checking.put(completed, checking.getOrDefault(completed, 0) - 1));
+        // 마라톤 선수가 완주한 여부를 체크하는 목록 (기본값 0, 동명이인은 +1)
+        Map<String, Integer> checking = new HashMap<>();
+        for (String player : participant) {
+            checking.put(player, checking.getOrDefault(player, 0) + 1);
+        }
+        
+        // 완주자 체킹 (완주자의 값 -1)
+        for (String completed : completion) {
+            checking.put(completed, checking.getOrDefault(completed, 0) - 1);
+        }
         
         // 완주하지 못한 사람 탐색
-        String answer = checking.entrySet().stream()
+        answer = checking.entrySet().stream()
                                 .filter(entry -> entry.getValue() > 0)
                                 .map(Map.Entry::getKey)
                                 .findFirst()
