327ac82053e6a670ceb83bf1dbf685b3e3e94905
Lee Dongmin <milkskfk12@gmail.com>
2025-11-17 21:46:58 +0900

    [level 3] Title: 정수 삼각형, Time: 14.34 ms, Memory: 65.4 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/README.md
+++ 프로그래머스/3/43105. 정수 삼각형/README.md
@@ -1,10 +1,10 @@
 # [level 3] 정수 삼각형 - 43105 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43105) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43105?language=java) 
 
 ### 성능 요약
 
-메모리: 65.4 MB, 시간: 12.74 ms
+메모리: 65.4 MB, 시간: 14.34 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 19일 21:11:33
+2025년 11월 17일 21:46:55
 
 ### 문제 설명
 
--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,9 +1,9 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[][] triangle) {
-        int answer = 0; // 거쳐간 숫자의 최댓값
-        int height = triangle.length;   // 삼각형의 높이
+        // 거쳐간 숫자의 최댓값
+        int answer = 0; 
+        // 삼각형의 높이
+        int height = triangle.length;
         
         // 삼각형의 높이가 1일 때
         if (height == 1) {
@@ -13,15 +13,21 @@ class Solution {
         // 삼각형 높이가 2이상일 때
         // 부모 노드 중 가장 큰 값을 더해가면서 내려감
         for (int i = 1; i < height; i++) {
-            for (int j = 0; j < i+1; j++) {
+            for (int j = 0; j < i + 1; j++) {
                 
-                if (j == 0) {   // 왼쪽 대각선으로 내려갈 때
-                    triangle[i][j] += triangle[i-1][j];
-                } else if (j == i) {    // 오른쪽 대각선으로 내려갈 때
-                    triangle[i][j] += triangle[i-1][j-1];
+                if (j == 0) {   
+                    // 왼쪽 대각선으로 내려갈 때
+                    triangle[i][j] += triangle[i - 1][j];
+                    
+                } else if (j == i) {    
+                    // 오른쪽 대각선으로 내려갈 때
+                    triangle[i][j] += triangle[i - 1][j - 1];
+                    
                 } else if (i > 0 && j < i) {
                     // 두 부모 노드 중에서 더 큰 값을 더함
-                    triangle[i][j] += Math.max(triangle[i-1][j-1], triangle[i-1][j]);   
+                    triangle[i][j] += Math.max(
+                        triangle[i - 1][j - 1], triangle[i - 1][j]
+                    );   
                 }
                 
                 // 최대값 갱신
