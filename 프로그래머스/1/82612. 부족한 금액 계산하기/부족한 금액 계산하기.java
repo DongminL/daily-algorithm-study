a0c82d09afd55de82d25ca25ad414ec2eabfeabb
Lee Dongmin <milkskfk12@gmail.com>
2026-05-13 22:32:45 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.10 ms, Memory: 61.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long solution(int price, int money, int count) {
+        long answer = -1 * money;
+        
+        for (int i = 1; i <= count; i++) {
+            answer += price * i;
+        }
+        
+        if (answer < 0) {
+            return 0L;
+        }
+        
+        return answer;
+    }
+}
