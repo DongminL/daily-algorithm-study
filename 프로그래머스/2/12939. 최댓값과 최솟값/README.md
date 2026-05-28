f741a80dc77dd8540f27ffe4e7a1f31664a4297a
Lee Dongmin <milkskfk12@gmail.com>
2026-05-28 20:51:35 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 5.90 ms, Memory: 62.6 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/README.md
+++ 프로그래머스/2/12939. 최댓값과 최솟값/README.md
@@ -1,10 +1,10 @@
 # [level 2] 최댓값과 최솟값 - 12939 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12939?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12939) 
 
 ### 성능 요약
 
-메모리: 76.4 MB, 시간: 2.53 ms
+메모리: 62.6 MB, 시간: 5.90 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 03일 19:36:08
+2026년 05월 28일 20:51:31
 
 ### 문제 설명
 
--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -2,24 +2,14 @@ import java.util.*;
 
 class Solution {
     public String solution(String s) {
-        StringBuilder answer = new StringBuilder();
-        int[] result = {Integer.MAX_VALUE, Integer.MIN_VALUE};  // {min, max}
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for (String num : s.split(" ")) {
+            min = Math.min(min, Integer.parseInt(num));
+            max = Math.max(max, Integer.parseInt(num));
+        }
         
-        Arrays.stream(s.split(" ")).map(Integer::parseInt).forEach(num -> {
-            // 최대값 구하기
-            if (num > result[1]) {
-                result[1] = num;
-            }
-            
-            // 최소값 구하기
-            if (num < result[0]) {
-                result[0] = num;
-            }
-        });
-        
-        // 결과 출력
-        answer.append(result[0]).append(" ").append(result[1]);
-        
-        return answer.toString();
+        return min + " " + max;
     }
 }
