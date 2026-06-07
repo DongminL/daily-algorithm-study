392a694967b4e4a0385130e5369505d79589b231
Lee Dongmin <milkskfk12@gmail.com>
2026-06-07 22:53:08 +0900

    [level 1] Title: 폰켓몬, Time: 2.57 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,22 +2,22 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        // 폰켓몬의 수
+        // 폰켓몬의 종류 번호의 개수
         int answer = nums.length;   
         
-        // 각 종류마다의 폰켓몬의 수 
+        // 각 종류마다의 개수 
         Map<Integer, Integer> count = new HashMap<>();
         for (int num : nums) {
             count.put(num, count.getOrDefault(num, 1) + 1);
         }
         
-        int typeCount = count.keySet().size();  // 폰켓몬 종류의 수
+        // 폰켓몬 종류 가지수
+        int typeCount = count.keySet().size();
         
-        // 가장 많은 종류의 폰켓몬을 선택할 때, 종류의 수
-        if (typeCount <= answer / 2) {
-            return typeCount;
-        }
+        // 가장 다양한 종류의 폰켓몬을 선택
+        answer = typeCount <= answer / 2 ? 
+            typeCount : answer / 2;
         
-        return answer / 2;
+        return answer;
     }
 }
