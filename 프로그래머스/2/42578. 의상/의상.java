fe9bd55d1470f60f7ed7efe05b41c91c03cd8e03
Lee Dongmin <milkskfk12@gmail.com>
2026-06-09 23:02:52 +0900

    [level 2] Title: 의상, Time: 0.49 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,12 +2,15 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
+        // 서로 다른 옷의 조합의 수
         int answer = 1;
         
-        // 종류별 옷의 개수 구하기
+        // 종류별 옷 개수 구하기
         Map<String, Integer> typeCount = new HashMap<>();
         for (int i = 0; i < clothes.length; i++) {
-            typeCount.put(clothes[i][1], typeCount.getOrDefault(clothes[i][1], 0) + 1);
+            typeCount.put(
+                clothes[i][1], typeCount.getOrDefault(clothes[i][1], 0) + 1
+            );
         }
         
         // 전체 경우의 수 구하기
