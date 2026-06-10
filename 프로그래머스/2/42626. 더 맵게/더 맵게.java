a6629709a68114557b4a611b0d1d7c7b00a9c200
Lee Dongmin <milkskfk12@gmail.com>
2026-06-10 11:59:52 +0900

    [level 2] Title: 더 맵게, Time: 711.15 ms, Memory: 154 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,9 +1,9 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int[] scoville, int K) {
-        if (K == 0) {
-            return K;
+    public int solution(int[] scoville, int k) {
+        if (k == 0) {
+            return k;
         }
         
         // 섞어야 하는 최소 횟수
@@ -11,25 +11,27 @@ class Solution {
         
         // 우선 순위 큐에 삽입
         PriorityQueue<Long> pq = new PriorityQueue<>();
-        Arrays.stream(scoville).mapToLong(Long::valueOf).forEach(pq::offer);
+        for (int v : scoville) {
+            pq.offer(Long.valueOf(v));
+        }
         
         // 두 스코빌 지수 섞기
         while (pq.size() > 1) {
             // 최소 스코빌이 K 이상이 될 때 종료
-            if (pq.peek() >= K) {
+            if (pq.peek() >= k) {
                 return answer;
             }
             
-            long first = pq.poll();  // 가장 맵지 않은 음식의 스코빌 지수
-            long second = pq.poll(); // 두 번째로 맵지 않은 음식의 스코빌 지수
-            long mix = second * 2 + first;   // 섞은 음식의 스코빌 지수
+            Long first = pq.poll();  // 가장 안 매운 음식의 스코빌 지수
+            Long second = pq.poll(); // 두 번째로 안 매운 음식의 스코빌 지수
+            Long mix = second * 2 + first;   // 섞은 음식의 스코빌 지수
             
             pq.offer(mix);
             answer++;
         }
         
         // K 이상으로 만들 수 없는 경우
-        if (pq.peek() < K) {
+        if (pq.peek() < k) {
             return -1;
         }
         
