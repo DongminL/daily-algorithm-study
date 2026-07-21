7768e1a072ca67a65b0e0816e17cb3509f5b1759
Lee Dongmin <milkskfk12@gmail.com>
2026-07-21 11:24:40 +0900

    [level 3] Title: 입국심사, Time: 19.44 ms, Memory: 92.9 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -3,7 +3,7 @@ import java.util.*;
 class Solution {
     public long solution(int n, int[] times) {
         long answer = 1;   // 모든 사람이 심사를 받는데 걸리는 시간의 최솟값
-        long max = 1_000_000_000L * n; // 최대값
+        long max = 1_000_000_000L * n;
         
         // 최소 소요 시간 탐색 (Binary Search)
         while (answer <= max) {
@@ -22,10 +22,11 @@ class Solution {
     
     // 모든 사람을 심사할 수 있는 시간인지 확인
     private boolean isValidTime(long time, int n, int[] times) {
-        long count = 0;  // 각 심사관이 time 내에 처리할 수 있는 사람의 수
+        // 각 심사관이 time 내에 처리할 수 있는 사람의 수
+        long count = 0;
         
         for (int t : times) {
-            count += time / t;    
+            count += time / t;
         }
         
         return count >= n;
